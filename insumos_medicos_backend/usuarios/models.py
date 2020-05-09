13d8ddd406b1d76d373235b77a45d4a9b2b2466b
Martin Ezequiel Gonzalez <martinegonzalez95@gmail.com>
2020-05-09 14:42:23 -0300

    Crea el modelo Usuario y la migracion asociada

--- insumos_medicos_backend/usuarios/models.py
+++ insumos_medicos_backend/usuarios/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=200)
+    email = models.CharField(max_length=200, unique=True)
+    telefono = models.CharField(max_length=200)
+    entidad = models.CharField(max_length=200)
+    cargo = models.CharField(max_length=200)
+    localidad = models.CharField(max_length=200)
+
+    # Muestra al Usuario de una manera mas elegante frente al usuario final.
+    # Utiliza el ID combinado con el email ya que ambos son valores únicos para todos los usuarios.
+    def __str__(self):
+        return str(self.id) + '.' + self.email
