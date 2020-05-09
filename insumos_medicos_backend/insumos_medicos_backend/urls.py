8b70a1f9dc220e629517c1a26c0362067a6d48f1
Martin Ezequiel Gonzalez <martinegonzalez95@gmail.com>
2020-05-09 13:53:30 -0300

    Crea app, ruta del index y vista del index de usuarios

--- insumos_medicos_backend/insumos_medicos_backend/urls.py
+++ insumos_medicos_backend/insumos_medicos_backend/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('usuarios/', include('usuarios.urls')),
     path('admin/', admin.site.urls),
 ]
