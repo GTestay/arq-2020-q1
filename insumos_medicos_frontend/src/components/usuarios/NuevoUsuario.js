eb4884ca700cadffd203967d3287b8c8133a93d9
Martin Ezequiel Gonzalez <martinegonzalez95@gmail.com>
2020-05-18 19:58:29 -0300

    Crea el componente para un nuevo usuario

---
+++ insumos_medicos_frontend/src/components/usuarios/NuevoUsuario.js
@@ -0,0 +1,10 @@
+import estilo from './nuevo-usuario.css'
+
+const NuevoUsuario = () => (
+    <div class={estilo.home}>
+        <h1>Nuevo Usuario</h1>
+        <p>Para crear un nuevo usuario utilizamos el siguiente formulario: </p>
+    </div>
+);
+
+export default NuevoUsuario;
