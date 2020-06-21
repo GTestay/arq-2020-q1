fd2d8e619a4c3b21ffc1b29ec7a7e0b67068dfd2
Gastón Testay <gaston.testay@gmail.com>
2020-06-21 17:14:33 -0300

    Agrega un par de logs a los POST

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 var app = require('./src/app');
+const logger = require('simple-node-logger').createSimpleLogger();
 
 app.listen(4000, () => {
-  console.log('Server inicializado en puerto 4000');
+  logger.info('Server inicializado en puerto 4000');
 });
