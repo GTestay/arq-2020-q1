7bc6f976efd316301336cd8d3c2cbcff2e332128
Gastón Testay <gaston.testay@gmail.com>
2020-06-21 17:24:58 -0300

    agrega que se guarde en un archivo

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 var app = require('./src/app');
-const logger = require('simple-node-logger').createSimpleLogger();
+const logger = require('simple-node-logger').createSimpleLogger('project.log');
 
 app.listen(4000, () => {
   logger.info('Server inicializado en puerto 4000');
