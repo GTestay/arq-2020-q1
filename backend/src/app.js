b34ff6d8ab02f9579f0365ac841f44f39a1f3f07
Gastón Testay <gaston.testay@gmail.com>
2020-07-05 20:18:28 -0300

    Agrega autenticación en el back
    * Usa JWT simple para generar y validar tokens
    
    * Agrega protección de rutas
    
    * Agrega mejores respuestas de error
    
    * Agrega dot-env para manejo de variables de entorno

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,77 +2,108 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const { logger, requestMiddleware } = require('../conf/logger');
+const jwt = require('jsonwebtoken');
+const dotEnv = require('dotenv').config().parsed;
 
 const Usuario = require('./modelos/usuario');
 const Solicitud = require('./modelos/solicitud');
 const repositorioUsuarios = require('./repositorios/repositorioUsuarios');
 const repositorioSolicitud = require('./repositorios/repositorioDeSolicitudes');
 
+app.set('jwtToken', dotEnv.JWT_TOKEN);
 app.use(express.json());
 app.use(cors());
 app.use(requestMiddleware);
 
-app.route('/usuarios')
+const rutasAutenticadas = express.Router()
+if(process.env.NODE_ENV !== 'test'){
+    rutasAutenticadas.use((req, res, next) => {
+        const token = req.headers['token-usuario'];
+        if (!!token) {
+            jwt.verify(token, app.get('jwtToken'), (err, decoded) => {
+                if(err) {
+                    respuestaDeError(res, 401, 'Ese token es inválido');
+                } else {
+                    req.decoded = decoded;
+                    next();
+                }
+            });
+        } else {
+            respuestaDeError(res, 401, 'No enviaste el token');
+        }
+    });
+}
+
+rutasAutenticadas.route('/usuarios')
     .get(async (req, res) => {
         const usuarios = await repositorioUsuarios.obtenerTodos();
 
         res.send(usuarios);
     })
-    
+
     .post(async (req, res) => {
-        logger.appInfo(`Usuario creado`);
         await repositorioUsuarios.agregar(new Usuario(req.body));
 
-        res.status(201);
-        res.send('{}');
-});
+        respuestaDeCreacion(res, 'Usuario creado');
+    });
 
-app.route('/solicitudes')
-    .get(async (req, res) => {
+rutasAutenticadas.route('/solicitudes')
+    .get( async (req, res) => {
         const solicitudes = await repositorioSolicitud.obtenerTodos();
-        
+
         res.send(solicitudes);
     })
-    
+
     .post(async (req, res) => {
-        logger.appInfo(`Solicitud creada`);
         await repositorioSolicitud.nueva(new Solicitud(req.body));
-        
-        res.status(201);
-        res.send('{}');
+
+       respuestaDeCreacion(res, 'Solicitud creada');
 });
 
-app.route('/solicitudes/:id/cancelar')
+rutasAutenticadas.route('/solicitudes/:id/cancelar')
     .patch(async (req, res) => {
         const id = req.params.id;
         const email = req.body.email;
         const solicitud = await repositorioSolicitud.cancelar({ id, email });
-    
+
         if(!!solicitud) {
             logger.appInfo(`Solicitud de ID ${id} del usuario ${email} ha sido cancelada`);
             res.send(solicitud);
         } else {
-            logger.appWarn(`Solicitud de ID ${id} del usuario ${email} no se ha podido cancelar`);
-            res.status(404);
-            res.send('No se pudo cancelar');
+            respuestaDeError(res, 404, `Solicitud de ID ${id} del usuario ${email} no se ha podido cancelar`);
         }
 });
 
-app.route('/login')
+const rutasSinAutenticacion = express.Router()
+
+rutasSinAutenticacion.route('/login')
     .post(async (req, res) => {
         const email = req.body.email;
         logger.appInfo(`Logueando a ${email}`);
 
         const usuario = await repositorioUsuarios.obtenerPorEmail(email);
-    
-        if(!!usuario) {
+
+    if(!!usuario) {
             logger.appInfo(`${email} se ha logueado satisfactoriamente`);
-            res.send(usuario);
+            res.send({ ...usuario, token: jwt.sign(usuario.toString(), dotEnv.JWT_TOKEN) });
         } else {
-            logger.appWarn(`${email} no se ha logueado satisfactoriamente`);
-            res.status(404);
-            res.send('No se encontró el usuario :(');
+            respuestaDeError(res, 404, `${email} no se ha logueado satisfactoriamente`);
         }
 });
+
+function respuestaDeCreacion(res, mensajeLog) {
+  logger.appInfo(mensajeLog);
+  res.status(201);
+  res.send('{}');
+}
+
+function respuestaDeError(res, status, mensaje) {
+    logger.appWarn(mensaje);
+    res.status(status);
+    res.send({ mensaje });
+}
+
+app.use('/', rutasSinAutenticacion)
+app.use('/', rutasAutenticadas)
 
 module.exports = app;
